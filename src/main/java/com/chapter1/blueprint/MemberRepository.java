179ed1eae58038dbbea182962e518bee1a5f2083
jisu <jisu0259@gmail.com>
2024-10-19 21:45:56 +0900

    feature member entitiy

---
+++ src/main/java/com/chapter1/blueprint/MemberRepository.java
@@ -0,0 +1,26 @@
+package com.chapter1.blueprint;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberRepository {
+
+    /*
+    * 컨테이너가 관리하는 엔티티 매니저를 빈에 주입합니다.
+    * 이를 통해 개발자가 직접 엔티티 매니저를 생성하고 관리할 필요 없이 컨테이너에 위임할 수 있습니다.
+    * */
+    @PersistenceContext
+    private EntityManager em;
+
+    public Long save(Member member) {
+        em.persist(member);
+        // 커맨드와 쿼리를 분리
+        return member.getId();
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+}
---
+++ src/test/java/com/chapter1/blueprint/MemberRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.chapter1.blueprint;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    // @Rollback(false) // transaction 처리 안 함, test가 기록에 남음
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
+    }
+}
