46aacc0fd8b037573a63dfece947a0f93436ccfc
kim jiwoo <annejwkim00@naver.com>
2024-11-12 00:41:02 +0900

    Feat: 금융 상품 추천 API 구현

--- src/main/java/com/chapter1/blueprint/finance/repository/SavingsListRepository.java
+++ src/main/java/com/chapter1/blueprint/finance/repository/SavingsListRepository.java
@@ -1,9 +1,15 @@
 package com.chapter1.blueprint.finance.repository;
 
+import com.chapter1.blueprint.finance.domain.LoanList;
 import com.chapter1.blueprint.finance.domain.SavingsList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SavingsListRepository extends JpaRepository<SavingsList, Long> {
+
+    @Query(value = "SELECT * FROM finance.savings_list ORDER BY intr_rate2 DESC LIMIT 1", nativeQuery = true)
+    SavingsList getSavingsFilter();
+
 }
