a7edd74440d7050f22b4607e6942f244fd288e42
kim jiwoo <annejwkim00@naver.com>
2024-11-16 20:14:41 +0900

    Fix: 정책 상세보기 API 수정

--- src/main/java/com/chapter1/blueprint/policy/repository/PolicyDetailRepository.java
+++ src/main/java/com/chapter1/blueprint/policy/repository/PolicyDetailRepository.java
@@ -1,10 +1,20 @@
 package com.chapter1.blueprint.policy.repository;
 
 import com.chapter1.blueprint.policy.domain.PolicyDetail;
+import com.chapter1.blueprint.policy.domain.dto.PolicyDetailDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PolicyDetailRepository extends JpaRepository<PolicyDetail, Long> {
+
+    @Query("SELECT new com.chapter1.blueprint.policy.domain.dto.PolicyDetailDTO(" +
+            "d.idx, d.subject, d.condition, d.content, d.scale, d.enquiry, d.document, d.url, d.way, " +
+            "f.minAge, f.maxAge, f.job) " +
+            "FROM PolicyDetail d JOIN PolicyDetailFilter f ON d.idx = f.idx " +
+            "WHERE d.idx = :idx")
+    PolicyDetailDTO findPolicyDetailByIdx(@Param("idx") Long idx);
 
 }
